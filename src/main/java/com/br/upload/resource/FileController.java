76ce846b760bb7282ca7d7aaacf5100135ee63f0
Fabiano dos Santos Silva <fabiano.asistemas@gmail.com>
2025-11-16 10:17:05 -0300

    ajuste logstack e config base dados

--- src/main/java/com/br/upload/resource/FileController.java
+++ src/main/java/com/br/upload/resource/FileController.java
@@ -1,6 +1,7 @@
 package com.br.upload.resource;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,7 +20,7 @@ public class FileController {
 		
 	}
 	
-	
+	@PostMapping("/upload")
 	public ResponseEntity<FileRecord> upload(MultipartFile file){		
 		return ResponseEntity.ok(service.saveFile(file));
 	}
